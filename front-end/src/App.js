92a743444b4aa541e4d15aefd55d22ff4d0630c5
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-03-29 23:32:00 -0300

    adiciona header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,50 @@
+import { Button, Card, Nav , Navbar, Container} from "react-bootstrap";
+import Logo from "./imagens/logo.png";
+import Share from "./imagens/Search.png";
+import Use from "./imagens/Solid.png";
+import SideBar from "./imagens/Vector.png"
+import ApenasLetra from "./imagens/apenasLetra.png"
+import Voltar from "./imagens/Voltas.png"
+
+function Header () {
+  return (
+    <>
+    <div id="Header">
+      <div className="HeaderDiv">
+          <img src={SideBar} width='28px' height='24px' className="SideBAr"/>
+          <img src={Logo} width='52px' height='52px' className="Logo"/>
+          <div className="ContainerShaHome"> 
+              <img src={Share} width="20px" heigth= "20px"/>
+              <img src={Use} width="20px" heigth= "20px"/>
+          </div>
+      </div>
+      {/* <div className="HeaderDivMin800px">
+        <img src={Logo} width='52px' height='52px' className="Logo"/>
+      </div> */}
+    </div>
+      {/* <nav className="NavBar">
+        <header>
+          <img src={Logo} width='52px' height='52px' className="Logo"/>
+          <img src={Voltar} width='31px' height='19px' className="Logo"/>
+        </header>
+      </nav> */}
+    </>
+  )
+}
+
+function HomePage () {
+  return (
+    <>
+      <Header/>
+      <div className="ImagemMund"><img src={ApenasLetra} width='265px' height='73px' /></div>
+    </>
+  )
+}
 
 function App() {
   return (
    <>
-      <p> kskskskssa</p>
+    <HomePage/>
    </>
   );
 }
