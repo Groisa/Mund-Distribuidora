d173919de00d1d69ed8ef1fc93e5556947542c46
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-04-08 19:58:53 -0300

    teste

--- front-end/src/Componentes/Cards/SecCategory/index.js
+++ front-end/src/Componentes/Cards/SecCategory/index.js
@@ -1,14 +1,67 @@
 import { Card } from "react-bootstrap"
 import ImagemTeste from '../../../imagens/teste.png'
-function SecCategory ({products}) {
+function SecCategory () {
     return(
         <section className="SecCategory ">
                 <Card className='CardConteiner bg-transparent'>
-                    <Card.Img variant="top" src={products.image} />
+                    <Card.Img variant="top" src={ImagemTeste} />
                     <Card.Body className='CardConteiners'>
-                        <Card.Title>{products.name}</Card.Title>
+                        <Card.Title>Nome Produto</Card.Title>
                         <Card.Text>
-                            {products.shortDescription}
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <div className="ButtomEl">
+                            <button>Orçar</button>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className='CardConteiner bg-transparent'>
+                    <Card.Img variant="top" src={ImagemTeste} />
+                    <Card.Body className='CardConteiners'>
+                        <Card.Title>Nome Produto</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <div className="ButtomEl">
+                            <button>Orçar</button>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className='CardConteiner bg-transparent'>
+                    <Card.Img variant="top" src={ImagemTeste} />
+                    <Card.Body className='CardConteiners'>
+                        <Card.Title>Nome Produto</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <div className="ButtomEl">
+                            <button>Orçar</button>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className='CardConteiner bg-transparent'>
+                    <Card.Img variant="top" src={ImagemTeste} />
+                    <Card.Body className='CardConteiners'>
+                        <Card.Title>Nome Produto</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <div className="ButtomEl">
+                            <button>Orçar</button>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className='CardConteiner bg-transparent'>
+                    <Card.Img variant="top" src={ImagemTeste} />
+                    <Card.Body className='CardConteiners'>
+                        <Card.Title>Nome Produto</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
                         </Card.Text>
                         <div className="ButtomEl">
                             <button>Orçar</button>
--- front-end/src/View/Products/index.js
+++ front-end/src/View/Products/index.js
@@ -2,44 +2,14 @@ import Layout from "../../Componentes/layout";
 import './index.css'
 import Box from '../../imagens/BoxProducts.png'
 import SecCategory from "../../Componentes/Cards/SecCategory";
-import { useEffect, useState } from "react";
-import { Alert } from "react-bootstrap";
-import Loading from "../../Componentes/Loading";
 
 function Products() {
-    const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [erroCatch, seterroCatch] =useState()
-    useEffect(() => {
-        fetch("http://localhost:3001/products")
-        .then((response) => response.json())
-        .then((data) => {
-            setProducts(data)
-        })
-        .finally(() => {
-            setLoading(false)
-        })
-        .catch (() => {
-            seterroCatch('Falha ao buscar classes de produtos. Tente Novamente')
-        })
-    }, [])
     return (
         <Layout>
             <div className="CompanentBox">
                 <img src={Box} alt='a' />
             </div>
-            {loading && (
-                <Loading/>
-            )}
-            {erroCatch && (
-                <Alert variant="danger">{erroCatch}</Alert>
-            )}
-            {products.map(products=> (
-                <div key={products.id}>
-                    <SecCategory products={products}/> 
-                </div>
-            ))}
-             
+            <SecCategory/>  
         </Layout>
     )
 }
