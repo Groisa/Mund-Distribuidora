409da9ce72edbfb52e1993fea717ee652691b78a
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-04-08 22:18:11 -0300

    adiciona favicon

--- front-end/src/Componentes/Cards/SecCategory/index.js
+++ front-end/src/Componentes/Cards/SecCategory/index.js
@@ -1,67 +1,13 @@
 import { Card } from "react-bootstrap"
-import ImagemTeste from '../../../imagens/teste.png'
-function SecCategory () {
+function SecCategory ({products}) {
     return(
         <section className="SecCategory ">
                 <Card className='CardConteiner bg-transparent'>
-                    <Card.Img variant="top" src={ImagemTeste} />
+                    <Card.Img variant="top" src={products.image} alt='a' />
                     <Card.Body className='CardConteiners'>
-                        <Card.Title>Nome Produto</Card.Title>
+                        <Card.Title>{products.name}</Card.Title>
                         <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <div className="ButtomEl">
-                            <button>Orçar</button>
-                        </div>
-                    </Card.Body>
-                </Card>
-                <Card className='CardConteiner bg-transparent'>
-                    <Card.Img variant="top" src={ImagemTeste} />
-                    <Card.Body className='CardConteiners'>
-                        <Card.Title>Nome Produto</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <div className="ButtomEl">
-                            <button>Orçar</button>
-                        </div>
-                    </Card.Body>
-                </Card>
-                <Card className='CardConteiner bg-transparent'>
-                    <Card.Img variant="top" src={ImagemTeste} />
-                    <Card.Body className='CardConteiners'>
-                        <Card.Title>Nome Produto</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <div className="ButtomEl">
-                            <button>Orçar</button>
-                        </div>
-                    </Card.Body>
-                </Card>
-                <Card className='CardConteiner bg-transparent'>
-                    <Card.Img variant="top" src={ImagemTeste} />
-                    <Card.Body className='CardConteiners'>
-                        <Card.Title>Nome Produto</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <div className="ButtomEl">
-                            <button>Orçar</button>
-                        </div>
-                    </Card.Body>
-                </Card>
-                <Card className='CardConteiner bg-transparent'>
-                    <Card.Img variant="top" src={ImagemTeste} />
-                    <Card.Body className='CardConteiners'>
-                        <Card.Title>Nome Produto</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                            {products.shortDescription}
                         </Card.Text>
                         <div className="ButtomEl">
                             <button>Orçar</button>
--- front-end/src/View/Products/index.js
+++ front-end/src/View/Products/index.js
@@ -2,14 +2,28 @@ import Layout from "../../Componentes/layout";
 import './index.css'
 import Box from '../../imagens/BoxProducts.png'
 import SecCategory from "../../Componentes/Cards/SecCategory";
+import { useEffect, useState } from "react";
+
 
 function Products() {
+    const [ProductsPrimary, setProductsPrimary] = useState([])
+    useEffect(() => {
+        fetch(' http://localhost:3001/products')
+            .then((response) => response.json())
+            .then((data) => {
+                setProductsPrimary(data)
+            })
+    }, [])
     return (
         <Layout>
             <div className="CompanentBox">
                 <img src={Box} alt='a' />
             </div>
-            <SecCategory/>  
+            {ProductsPrimary.map(products => (
+                <div className="ProductsCategory" key={products.id}>
+                    <SecCategory products={products} />
+                </div>
+            ))}
         </Layout>
     )
 }
