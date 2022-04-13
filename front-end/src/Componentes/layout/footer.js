2548b719c8a54d7526c57eea4bbd814ece1abcd0
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-04-12 21:35:25 -0300

    retira cross-end

--- front-end/src/Componentes/layout/footer.js
+++ front-end/src/Componentes/layout/footer.js
@@ -1,6 +1,10 @@
+
 function Footer () {
     return (
       <>
+        <footer className='footer text-center text-black py-2 mt-4'>
+          <a href="https://www.instagram.com/jovem._dev/" target="_blank"><i class="bi bi-instagram"> </i></a>
+        </footer>
       </>
     )
   }
