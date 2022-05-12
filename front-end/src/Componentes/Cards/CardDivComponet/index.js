204d42a23c82fbe08956889e16f7871034f2cc66
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-05-11 23:13:32 -0300

    mostra id produto somente para admins

--- front-end/src/Componentes/Cards/CardDivComponet/index.js
+++ front-end/src/Componentes/Cards/CardDivComponet/index.js
@@ -1,18 +1,28 @@
+import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import { UserType } from "../../../Constantes/typeUser"
+import { selectUser } from "../../../store/Users/user.selctor"
 
 
 
-function CardDivComponet({products}) {
+function CardDivComponet({ products }) {
+    const user = useSelector(selectUser)
     return (
         <section className="SecCategory">
             <div className="DivCard">
                 <div><img src={products.image} alt='a' /></div>
                 <div className="ItensConteines">
-                    <h2> {products.name} #{products.id}</h2>
+                    <h2>
+                        {products.name}
+                        {user && user.type === UserType.admin 
+                            &&
+                            <> # {products.id} </>
+                        }
+                    </h2>
                     <p>{products.shortDescription} </p>
                     <Link to={`/produtos/${products.id}`} className="ResetLink"><button> Ir para</button></Link>
                 </div>
-            </div> 
+            </div>
         </section>
     )
 }
