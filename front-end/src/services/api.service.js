6871f7728cfa04116b74856b876b2c13b5048218
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-05-13 21:42:03 -0300

    implementa header de altorizacao requisicao tipo post

--- front-end/src/services/api.service.js
+++ front-end/src/services/api.service.js
@@ -1,1 +1,15 @@
+import { store } from "../store/store"
+import { selectUser } from "../store/Users/user.selctor"
+
 export const urlApi = process.env.REACT_APP_API_URL
+
+export const getAuthorizationHeaders = () => {
+    const state = store.getState()
+    const user = selectUser(state)
+    if (user) {
+      return {
+        'authorization': `Bearer ${user.accessToken}`
+      }
+    }
+    return {}
+  }
